779e29216703a8ef105ab58f352b1f758e027777
Venkatesh Manohar <venkatesh.manohar@gmail.com>
2021-04-04 19:43:58 +0530

    Apr 4th

--- src/com/ven/leetcode/week235/MinimumAbsoluteSumDifference.java
+++ src/com/ven/leetcode/week235/MinimumAbsoluteSumDifference.java
@@ -1,5 +1,6 @@
 package com.ven.leetcode.week235;
 
+import java.util.Arrays;
 import java.util.TreeSet;
 
 /**
@@ -49,9 +50,7 @@ import java.util.TreeSet;
 public class MinimumAbsoluteSumDifference {
 	public int minAbsoluteSumDiff(int[] nums1, int[] nums2) {
 		int MOD = 1000000007;
-		int maxIndex = -1;
 		int maxDiff = Integer.MIN_VALUE;
-		int num1 = 0;
 		int num2 = 0;
 		int abs = 0;
 		TreeSet<Integer> s = new TreeSet<>();
@@ -59,9 +58,7 @@ public class MinimumAbsoluteSumDifference {
 			int diff = Math.abs(nums1[i] - nums2[i]);
 			s.add(nums1[i]);
 			if (diff > maxDiff) {
-				maxIndex = i;
 				maxDiff = diff;
-				num1 = nums1[i];
 				num2 = nums2[i];
 			}
 			abs = (abs + diff) % MOD;
@@ -82,5 +79,35 @@ public class MinimumAbsoluteSumDifference {
 
 		}
 		return abs;
+	}
+
+	public int minAbsoluteSumDiffOptimized(int[] nums1, int[] nums2) {
+		int[] sorted1 = nums1.clone();
+		Arrays.sort(sorted1);
+		int result = 0;
+		int save = 0;
+		for (int i = 0; i < nums1.length; i++) {
+			result += Math.abs(nums1[i] - nums2[i]);
+			result %= 1000000007;
+		}
+		for (int i = 0; i < nums1.length; i++) {
+			if (nums1[i] == nums2[i])
+				continue; // already equal, diff is already minimized
+			int index = Arrays.binarySearch(sorted1, nums2[i]);
+			int diff = Math.abs(nums1[i] - nums2[i]);
+			// nums2[i] is actually in nums1[i], so we can minimize the diff to 0!
+			if (index >= 0) {
+				save = Math.max(save, diff);
+			} else {
+				// find the closest number in nums1 that is to nums2[i], need to look 2 numbers
+				// around the found index(supposed insertion index)
+				int actualIndex = -1 * (index + 1);
+				if (actualIndex > 0)
+					save = Math.max(save, diff - Math.abs(nums2[i] - sorted1[actualIndex - 1]));
+				if (actualIndex < nums1.length)
+					save = Math.max(save, diff - Math.abs(nums2[i] - sorted1[actualIndex]));
+			}
+		}
+		return (result - save) % 1000000007;
 	}
 }
